c5a71576019d5ca2dffe38b5cff89ec23988d9b5
Gian Avila <avila.gian111@gmail.com>
2024-12-23 22:08:23 +0700

    fix: correct variable names and comments for urban and suburban path loss calculations

--- okumura_hata_area.m
+++ okumura_hata_area.m
@@ -11,20 +11,20 @@ fc = 1500; % Frequency (150 MHz hingga 1500 MHz)
 ahr_urban = 3.2*(log10(11.75*hr)).^2 - 4.97;
 
 % --- Menghitung Path Loss untuk Daerah Urban Large ---
-large_urban = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(ht)) * log10(d) - 13.82*log10(ht) - ahr;
+large_urban = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(ht)) * log10(d) - 13.82*log10(ht) - ahr_urban;
 
 %% Sub-Urban City
 % --- Menghitung Faktor Koreksi Ketinggian Antena ---
-ahr_subrural = ((1.1 * (log10(fc))) - 0.7)*hr - ((1.56*log10(fc))-0.8);
+ahr_suburban = ((1.1 * (log10(fc))) - 0.7)*hr - ((1.56*log10(fc))-0.8);
 
 % --- Menghitung Path Loss untuk Daerah Sub-Urban Medium Size ---
-medium_urban = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(ht)) * log10(d) - 13.82*log10(ht) - ahr_subrural;
+medium_urban = 69.55 + 26.16*log10(fc) + (44.9 - 6.55*log10(ht)) * log10(d) - 13.82*log10(ht) - ahr_suburban;
 
 % --- Menghitung Path Loss untuk Daerah Sub-Urban ---
 sub_urban = medium_urban - 2*(log10(fc/28)).^2 - 5.4;
 
 %% Rural Area
-% --- Menghitung Path Loss untuk Daerah Sub-Urban ---
+% --- Menghitung Path Loss untuk Daerah Rural ---
 rural = medium_urban - 4.78*(log10(fc))^2 + 18.733*log10(fc) - 40.98;
 
 %% --- Plot Hasil Perhitungan Loss ---
